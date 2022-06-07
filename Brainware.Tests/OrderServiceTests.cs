36030fb4c691cd9bfa39cd9c7d46f3de3aeed8a7
Michael Clagett <mclagett@hotmail.com>
2022-06-07 00:57:54 -0700

    Added more expected results to first high-level integration test (added expected product ids).

--- Brainware.Tests/OrderServiceTests.cs
+++ Brainware.Tests/OrderServiceTests.cs
@@ -19,9 +19,30 @@ namespace Brainware.Tests
 
             // Assert
             Assert.Equal(3, result.Count);
-            Assert.Contains(result, o => o.OrderId == 1 && o.Description.Contains("Our first order") && o.OrderProducts.Count == 3 && o.OrderTotal == 39.50M);
-            Assert.Contains(result, o => o.OrderId == 2 && o.Description.Contains("Our Second order") && o.OrderProducts.Count == 4 && o.OrderTotal == 44.00M);
-            Assert.Contains(result, o => o.OrderId == 3 && o.Description.Contains("Our third order") && o.OrderProducts.Count == 5 && o.OrderTotal == 44.25M);
+            Assert.Contains(result, o => o.OrderId == 1 && 
+                                        o.Description.Contains("Our first order") && 
+                                        o.OrderProducts.Count == 3 && 
+                                        o.OrderProducts.Exists(p => p.ProductId == 1) &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 3) &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 4) &&
+                                        o.OrderTotal == 39.50M);
+            Assert.Contains(result, o => o.OrderId == 2 && 
+                                        o.Description.Contains("Our Second order") && 
+                                        o.OrderProducts.Count == 4 &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 1) &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 2) &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 3) &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 5) &&
+                                        o.OrderTotal == 44.00M);
+            Assert.Contains(result, o => o.OrderId == 3 && 
+                                        o.Description.Contains("Our third order") && 
+                                        o.OrderProducts.Count == 5 &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 1) &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 2) &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 3) &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 4) &&
+                                        o.OrderProducts.Exists(p => p.ProductId == 5) &&
+                                        o.OrderTotal == 44.25M);
         }
     }
 }
